f03c2889f808d4bc2110a3c4f952ee4ca32ebff2
Matheus B <matheus_ob59@hotmail.com>
2022-06-02 03:00:19 -0300

    Fix urls.

--- frontend/src/utils/urls.js
+++ frontend/src/utils/urls.js
@@ -1,3 +1,3 @@
-export const newProductUrl = "http://scandiplatform.000webhostapp.com/api/resource/newProduct.php";
-export const allProductsUrl = "http://scandiplatform.000webhostapp.com/api/resource/allProducts.php";
-export const massDeleteUrl = "http://scandiplatform.000webhostapp.com/api/resource/massDelete.php";
+export const newProductUrl = "https://scandiplatform.000webhostapp.com/api/resource/newProduct.php";
+export const allProductsUrl = "https://scandiplatform.000webhostapp.com/api/resource/allProducts.php";
+export const massDeleteUrl = "https://scandiplatform.000webhostapp.com/api/resource/massDelete.php";
